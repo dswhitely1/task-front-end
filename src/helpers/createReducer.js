e44b62ead68e5fe35a4cdc3dcb17e13139f2d28e
Donald Whitely <dswhitely1@gmail.com>
2019-08-14 22:34:18 -0400

    Reducer Helper Function Created

---
+++ src/helpers/createReducer.js
@@ -0,0 +1,5 @@
+export const createReducer = (initialState, fnMap) => (
+    state = initialState, { type, payload }) => {
+  const handler = fnMap[type];
+  return handler ? handler(state, payload) : state;
+};
